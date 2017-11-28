ef494957c63dcfa0c873094690df874f0ebceb48
Ananya Jana <ananya.jana@gmail.com>
2017-11-28 19:28:43 +0530

    initialcheck-in

---
+++ src/heap.c
@@ -0,0 +1,17 @@
+/* Implementing Max-heap, Min-heap 
+Routines for Max-heap
+Max-Heapify - O(log n)
+Build Max-heap - linear
+Heap sort - O(nlog n)
+Max-heap insert, 
+Heap-extract max
+Heap-increase-key
+Heap-maximum - O(log n)
+*/
+
+#include < stdio.h>
+
+int main()
+{
+	
+}
