543b5d3cbace6fd808d0d77a86243009632b607f
Ananya Jana <ananya.jana@gmail.com>
2017-12-28 11:38:01 +0530

    incompleteversion

--- src/heap.c
+++ src/heap.c
@@ -14,42 +14,67 @@ Heap-maximum - O(log n)
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PARENT(i)	(i/2)
-#define LEFT(i)		(2*i)
-#define	RIGHT(i)	(2*i + 1)
+#define PARENT(i)	((i - 1)/2)
+#define LEFT(i)		(2*i + 1)
+#define	RIGHT(i)	(2*i + 2)
 
-void max_heapify(int a[], int n, int i);
-void build_max_heap(int a[], int n);	
+void max_heapify(int a[], int i);
+void build_max_heap(int a[]);	
 
+//heap size
+int size;
 
 int main()
 {
-	int n, i;
+	int i;
 	int *arr = NULL;
 	
 	//take user input: size of the array
 	printf("Enter the heap size: ");
-	scanf("%d", &n);
+	scanf("%d", &size);
 	
 	//create a dynamic array of size n
-	arr = (int*)malloc(n*sizeof(int));
+	arr = (int*)malloc(size*sizeof(int));
 	
 	//take input the individual elements
 	printf("Enter the heap elements:\n");
-	for(i = 0; i < n; ++i){
+	for(i = 0; i < size; ++i){
 		scanf("%d", &arr[i]);
 	}
-	for(i = 0; i < n; ++i){
+	for(i = 0; i < size; ++i){
 		printf("%d ", arr[i]);
 	}
 	
 	//build max heap with the user input elements
-	build_max_heap(arr, n);	
+	build_max_heap(arr);	
 	
 	free(arr);
 }
 
-void build_max_heap(int a[], int n)
+void build_max_heap(int a[])
 {
 	int i;
+	for(i = (size - 1)/2; i >= 0; --i)
+		max_heapify(a, i);
+}
+
+void max_heapify(int a[], int i)
+{
+	int l, r, largest, temp;
+	l = LEFT(i);
+	r = RIGHT(i);
+	
+	if((l < size) && (a[l] > a[i]))
+		largest = l;
+	else
+		largest = i;
+	if((r < size) && ( a[r] > a[largest]))
+		largest = r;
+	if(largest != i){
+		temp = a[largest];
+		a[largest] = a[i];
+		a[i] = temp;
+	}
+	
+	max_heapify(a, largest);
 }
