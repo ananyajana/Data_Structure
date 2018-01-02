506817802490f18a6a814039dbf0f6f610898984
Ananya Jana <ananya.jana@gmail.com>
2018-01-02 09:04:18 +0530

    working version

--- src/heap.c
+++ src/heap.c
@@ -53,6 +53,7 @@ int main()
 	for(i = 0; i < size; ++i){
 		printf("%d ", arr[i]);
 	}
+	printf("\n");
 	
 	//free the dynamic array
 	free(arr);
@@ -62,7 +63,7 @@ void build_max_heap(int a[])
 {
 	int i;
 	for(i = (sz - 1)/2; i >= 0; --i){
-		printf("i = %d\n", i);
+		//printf("i = %d\n", i);
 		max_heapify(a, i);
 	}	
 }
@@ -73,7 +74,7 @@ void max_heapify(int a[], int i)
 	l = LEFT(i);
 	r = RIGHT(i);
 
-	printf("%d %d\n", a[i], i);	
+	//printf("%d %d\n", a[i], i);	
 		
 	if((l < sz) && (a[l] > a[i]))
 		largest = l;
@@ -85,11 +86,11 @@ void max_heapify(int a[], int i)
 		temp = a[largest];
 		a[largest] = a[i];
 		a[i] = temp;
-		printf("hi\n");
+		/*printf("hi\n");
 		for(i = 0; i < size; ++i){
 			printf("%d ", a[i]);
 		}
-		printf("\n");
+		printf("\n");*/
 		max_heapify(a, largest);
 	}	
 }
@@ -107,19 +108,21 @@ void heap_sort(int a[])
 	
 	//place the biggest element which is at the root, at the end of the array
 	for(i = sz - 1; (i >= 1) && (sz > 1); --i){
+		//printf("1.sz = %d, i = %d\n", sz, i);
 		temp = a[0];
 		a[0] = a[i];
 		a[i] = temp;
 
-		printf("hi1\n");
+		/*printf("hi1\n");
 		for(i = 0; i < size; ++i){
 			printf("%d ", a[i]);
 		}
-		printf("\n");
+		printf("\n");*/
 		//reduce the size of the heap as we removed the largest element
 		--sz;
 	
 		//create a heap out of the remaining elements in the heap
 		max_heapify(a, 0);
+		//printf("2.sz = %d, i = %d\n", sz, i);
 	}
 }
